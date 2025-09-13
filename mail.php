f7dc5487cfdb7dba919294f00b64b39f1d0757da
Lushiness <lucy.gichu@strathmore.edu>
2025-09-13 15:16:12 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,24 +4,16 @@ use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
 require 'vendor/autoload.php';
-
-/**
- * Send a welcome email to a newly registered user
- * @param string $userEmail The email address of the user
- * @param string $userName The name of the user
- * @return bool True if email sent successfully, false otherwise
- */
 function sendWelcomeEmail($userEmail, $userName) {
     $mail = new PHPMailer(true);
 
     try {
-        // Server settings
-        $mail->SMTPDebug = 0; // Disable debug output
+        $mail->SMTPDebug = 0;
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'lucy.gichu@strathmore.edu'; // Your Gmail
-        $mail->Password   = 'vdvx wapn dcet ykxx';       // App Password
+        $mail->Username   = 'lucy.gichu@strathmore.edu';
+        $mail->Password   = 'vdvx wapn dcet ykxx';     
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port       = 465;
 
@@ -39,24 +31,19 @@ function sendWelcomeEmail($userEmail, $userName) {
             </head>
             <body>
                 <h1>Hello $userName,</h1>
-                <p>Welcome to TaskApp! We're excited to have you on board.</p>
-                <p>You can now start managing your tasks efficiently.</p>
-                <p>If you have any questions, feel free to contact us.</p>
-                <p>Best regards,<br>TaskApp Team</p>
+                <p>Welcome to TaskApp!</p>
             </body>
             </html>
         ";
-        $mail->AltBody = "Hello $userName,\n\nWelcome to TaskApp! We're excited to have you on board.\n\nYou can now start managing your tasks efficiently.\n\nIf you have any questions, feel free to contact us.\n\nBest regards,\nTaskApp Team";
+        $mail->AltBody = "Hello $userName,\n\nWelcome to TaskApp! I'm happy to have you!";
 
         $mail->send();
         return true;
     } catch (Exception $e) {
-        // Log error or handle it
         error_log("Email could not be sent. Error: {$mail->ErrorInfo}");
         return false;
     }
 }
 
-// Example usage (remove or comment out in production)
-// sendWelcomeEmail('user@example.com', 'John Doe');
 ?>
+
